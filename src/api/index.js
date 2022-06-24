86c0ce06fb97fd254238cb55c37bd42962a83c6a
ljsinsist20 <2593161116@qq.com>
2022-06-24 19:41:05 +0800

    4.3-搜索防抖

--- src/api/index.js
+++ src/api/index.js
@@ -3,75 +3,82 @@ import request from '@/utils/request'
 
 // 登录
 export const loginAPI = ({
-        mobile,
-        code
-    }) => {
-        return request({
-            url: '/v1_0/authorizations',
-            method: 'POST',
-            data: {
-                mobile,
-                code
-            }
-        })
-    }
-    // 获取所有频道列表
-export const allChannelListAPI = () => {
-        return request({
-            url: '/v1_0/channels'
-        })
-    }
-    // 设置用户的频道（重置式）
-export const updateChannelListAPI = ({ channels }) => {
-        return request({
-            url: '/v1_0/user/channels',
-            method: 'PUT',
-            data: {
-                channels: channels
-            }
-        })
-    }
-    // 获取用户的频道
-export const userChannelAPI = () => {
-        return request({
-            url: '/v1_0/user/channels'
+    mobile,
+    code
+}) => {
+    return request({
+        url: '/v1_0/authorizations',
+        method: 'POST',
+        data: {
+            mobile,
+            code
+        }
+    })
+}
 
-        })
-    }
-    // 删除指定用户频道
+// 获取所有频道列表
+export const allChannelListAPI = () => {
+    return request({
+        url: '/v1_0/channels'
+    })
+}
+
+// 设置用户的频道（重置式）
+export const updateChannelListAPI = ({ channels }) => {
+    return request({
+        url: '/v1_0/user/channels',
+        method: 'PUT',
+        data: {
+            channels: channels
+        }
+    })
+}
+
+// 获取用户的频道
+export const userChannelAPI = () => {
+    return request({
+        url: '/v1_0/user/channels'
+
+    })
+}
+
+// 删除指定用户频道
 export const deleteChannelAPI = ({ target }) => {
-        return request({
-            url: `/v1_0/user/channels/${target}`,
-            method: 'DELETE'
-        })
-    }
-    // 获取文章新闻推荐
+    return request({
+        url: `/v1_0/user/channels/${target}`,
+        method: 'DELETE'
+    })
+}
+
+// 获取文章新闻推荐
 export const artcilesListAPI = ({ channelId, timestamp }) => {
-        return request({
-            url: '/v1_0/articles',
-            // headers: {
-            //     Authorization: 'Bearer ' + store.state.token
-            // },
-            params: {
-                channel_id: channelId,
-                timestamp: timestamp || (new Date()).getTime()
-            }
-        })
-    }
-    // 对文章不喜欢
+    return request({
+        url: '/v1_0/articles',
+        // headers: {
+        //     Authorization: 'Bearer ' + store.state.token
+        // },
+        params: {
+            channel_id: channelId,
+            timestamp: timestamp || (new Date()).getTime()
+        }
+    })
+}
+
+// 对文章不喜欢
 export const articleDislikesAPI = ({ target }) => {
-        return request({
-            url: '/v1_0/article/dislikes',
-            method: 'POST',
-            data: {
-                target: target
-            }
-            // headers: {
-            //     Authorization: 'Bearer ' + store.state.token
-            // }
-        })
-    }
-    // 举报文章
+    return request({
+        url: '/v1_0/article/dislikes',
+        method: 'POST',
+        data: {
+            target: target
+        }
+        // headers: {
+        //     Authorization: 'Bearer ' + store.state.token
+        // }
+    })
+}
+
+// 举报文章
 export const articleReportAPI = ({ target, type, remark }) => {
     return request({
         url: '/v1_0/article/reports',
@@ -81,8 +88,15 @@ export const articleReportAPI = ({ target, type, remark }) => {
             type: type,
             remark: remark
         }
-        // headers: {
-        //     Authorization: 'Bearer ' + store.state.token
-        // }
+    })
+}
+
+// 获取联想建议（自动补全）
+export const suggestListAPI = ({ q }) => {
+    return request({
+        url: '/v1_0/suggestion',
+        query: {
+            q: q
+        }
     })
 }
--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    <van-search v-fofo placeholder="请输入搜索关键词" v-model="value" background="blue" show-action @cancel="$router.back()"/>
+    <van-search v-fofo placeholder="请输入搜索关键词" v-model="kw" background="blue" show-action @input="onInput"
+      @cancel="$router.back()" />
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      kw: '',
+      timer: null,
+      suggestList: []
+    }
+  },
+  methods: {
+    onInput () {
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        if (this.kw.length !== 0) {
+          console.log(this.kw)
+        }
+      }, 500)
+    }
+  }
 }
 </script>
 
